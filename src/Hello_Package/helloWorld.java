c92ee12a1d25fcb70fb535ef5f98f5be8970929b
Somi Ghomashchi <somi.taha2014@gmail.com>
2019-10-19 11:44:56 +0330

    Added date method

--- src/Hello_Package/helloWorld.java
+++ src/Hello_Package/helloWorld.java
@@ -1,8 +1,12 @@
 package Hello_Package;
+
+import java.util.Date;
+
 //Author: Somayeh Ghomashchi
 public class helloWorld {
     public static void main (String args[]){
 
         System.out.println("Hello World");
+        System.out.println( new Date());
     }
 }
