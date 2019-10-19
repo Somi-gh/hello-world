d6355e5adbda7815c4cf89a2262220413a776ae1
Somi Ghomashchi <somi.taha2014@gmail.com>
2019-10-19 11:41:56 +0330

    print hello world

--- src/Hello_Package/helloWorld.java
+++ src/Hello_Package/helloWorld.java
@@ -1,5 +1,8 @@
 package Hello_Package;
 //Author: Somayeh Ghomashchi
 public class helloWorld {
-    
+    public static void main (String args[]){
+
+        System.out.println("Hello World");
+    }
 }
